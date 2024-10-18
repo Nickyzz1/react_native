01f51bff685d2a22c3c9b3d2d76bab40ccf57c6e
Nickyzz1 <nicolle.rodrigues.siqueira@gmail.com>
2024-10-18 16:21:46 -0300

    estilizações1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # react_native
 // não estamos configurando rotas, estamos editando elas
 npx expo install expo-font
+//mobbins
 flutter color extensão para ver cor
--- app/index.tsx
+++ app/index.tsx
@@ -1,88 +1,108 @@
-import { View, Text, SafeAreaView, TextInput, StyleSheet,TouchableOpacity } from "react-native";
-import { useState } from "react";
-import { Link, router} from "expo-router";
-
-export default function Login(){
-
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-
+import {
+    View,
+    Text,
+    SafeAreaView,
+    TextInput,
+    StyleSheet,
+    TouchableOpacity,
+  } from "react-native";
+  import { useState } from "react";
+  import { Link, router } from "expo-router";
+  
+  export default function Login() {
+    const [email, setEmail] = useState("");
+    const [pass, setPass] = useState("");
+  
     const onPress = () => {
-        router.push("../(tabs)")
-    }
-
+      router.push("../(tabs)");
+    };
+  
     console.log(email, pass);
     console.log(typeof email, typeof pass);
+  
+    return (
+      <View style={styles.container}>
 
-    return(
-        <>
-        <View style={styles.container} >
-            <SafeAreaView style = {styles.content}>
-                <Text style={styles.label} >Email</Text>
-                    <TextInput
-                        style={styles.input}
-                        onChangeText={setEmail}
-                        value={email}
-                        placeholder="Digite seu email"
-                        keyboardType="email-address"
-                    />
-                <Text style={styles.label}>Senha</Text>
-                    <TextInput
-                        onChangeText={setPass}
-                        value={pass}
-                        placeholder="Digite sua senha"
-                        keyboardType="numeric"
-                        secureTextEntry
-                        style={styles.input}
-                    />
-          
-                <TouchableOpacity style={styles.button} onPress={onPress}>
-                    <Text>Entrar</Text>
-                </TouchableOpacity>
-            
-                <View>
-                    <Link href={"/register"}>cadastrar novo user</Link>
-                </View>
+        <View style={styles.content}>
 
-            </SafeAreaView>
+        <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Digite seu email"
+            keyboardType="email-address"
+        />
+
+         <TextInput
+            onChangeText={setPass}
+            value={pass}
+            placeholder="Digite sua senha"
+            keyboardType="numeric"
+            secureTextEntry
+            style={styles.input}
+        />
+
+     
+
         </View>
-        </>
-    )
-}
-
-const styles  = StyleSheet.create({
-    input: {
-        borderRadius: 7,
-        padding: 5,
-        margin: 5,
-        backgroundColor: "#ffffff"
-    },
-    label:
-    {
-        color: "#ffffff",
-    },
-
-    content:
-    {
-       margin: 20,
-       padding: 10,
-       backgroundColor: "#252525",
-       borderRadius: 10
-    },
+        {/* <SafeAreaView style={styles.content}>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={setEmail}
+            value={email}
+            placeholder="Digite seu email"
+            keyboardType="email-address"
+          />
+          <Text style={styles.label}>Senha</Text>
+          <TextInput
+            onChangeText={setPass}
+            value={pass}
+            placeholder="Digite sua senha"
+            keyboardType="numeric"
+            secureTextEntry
+            style={styles.input}
+          />
+        </SafeAreaView>
+  
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text>Entrar</Text>
+        </TouchableOpacity>
+  
+        <View>
+          <Link href={"/register"}>cadastrar novo user</Link>
+        </View> */}
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
     container: {
-        // flex: 1,
-        display: "flex",
-        justifyContent: 'center',
-        alignItems: "center"
-    
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center",
       },
+    content: {
+        margin: 20,
+        padding: 20,
+        backgroundColor: "#252525",
+        borderRadius: 10,
+        minWidth: "auto",
+        width: 350
+      },
+    input: {
+      borderRadius: 7,
+      padding: 10,
+      margin: 5,
+      backgroundColor: "#ffffff",
+    },
+    label: {
+      color: "#ffffff",
+    },
     button: {
-        alignItems: 'center',
-        backgroundColor: '#DDDDDD',
-        padding: 10,
+      alignItems: "center",
+      backgroundColor: "#DDDDDD",
+      padding: 10,
     },
-    countContainer: {
-        alignItems: 'center',
-        padding: 10,
-    },
-})
+  });
+  
